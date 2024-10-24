791a1992e1ab8b26e13f0678d3ed018bff861d93
Duckiee <duc.nc@atomsolution.com.vn>
2024-10-24 11:51:17 +0700

    feat(use-debounce): add initial value

--- src/hooks/use-debounce.ts
+++ src/hooks/use-debounce.ts
@@ -1,16 +1,16 @@
 import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
-type Value = string | number
-type UseDebounce = [Value, Dispatch<SetStateAction<Value>>, Value]
+type UseDebounce<T> = [T, Dispatch<SetStateAction<T>>, T]
+type InitialValue<T> = T | (() => T)
 
-export function useDebounce(delay = 500): UseDebounce {
-  const [value, setValue] = useState<Value>('')
-  const [debounceValue, setDebounceValue] = useState<Value>('')
+export function useDebounce<T>(initialValue: InitialValue<T>, delay = 500): UseDebounce<T> {
+  const [value, setValue] = useState<T>(initialValue)
+  const [debounceValue, setDebounceValue] = useState<T>(initialValue)
 
   useEffect(() => {
     const handler = setTimeout(() => setDebounceValue(value), delay)
     return () => clearTimeout(handler)
-  })
+  }, [value, delay])
 
   return [value, setValue, debounceValue]
 }
